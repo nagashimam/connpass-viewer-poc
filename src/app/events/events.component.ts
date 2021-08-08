5c6c834f89fc35d3aef4116ffced01e68168e694
masato <masato.ngsm@gmail.com>
2021-08-08 12:24:14 +0900

    イベントを表示する

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { EventsQuery } from '../state/events.query';
 import { EventsService } from '../state/events.service';
+import { Event } from "../state/event.model";
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent {
   fromPlaceholder: string;
   toPlaceholder: string;
+  events$: Observable<Event[]>
 
-  constructor(private eventsService: EventsService) {
+  constructor(private eventsService: EventsService, private eventsQuery: EventsQuery) {
     const today = new Date();
     this.fromPlaceholder = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`
     this.toPlaceholder = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate() + 7}`
+
+    this.events$ = this.eventsQuery.selectAll();
   }
 
   searchForm = new FormGroup({
@@ -23,12 +29,12 @@ export class EventsComponent implements OnInit {
     to: new FormControl()
   });
 
-  ngOnInit(): void {
-
-  }
-
   search() {
     this.eventsService.get("Angular", new Date(2021, 7, 1), new Date(2021, 9, 1)).subscribe((response) => { console.log(response) })
+  }
+
+  trackById(_: number, value: Event): number | null {
+    return value ? value.event_id : null;
   }
 
 }
