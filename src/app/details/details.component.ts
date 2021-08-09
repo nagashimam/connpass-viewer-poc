bf7f5d26830cb8a97eafdb143e4b22c432979a84
masato <masato.ngsm@gmail.com>
2021-08-09 09:15:15 +0900

    全ページからのデータを引き継ぐ

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Observable, of } from 'rxjs';
+import { EventsQuery } from '../state/events.query';
+import { Event } from "../state/event.model";
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
+  // FIXME: undefinedのときどうするか考える(ありえないはずだけど)
+  event$: Observable<Event | undefined> = of();
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.queryParams.subscribe(params => console.log(params));
+  constructor(private activatedRoute: ActivatedRoute, private eventsQuery: EventsQuery) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.event$ = this.eventsQuery.selectEntity(params.event_id);
+    });
   }
-
-  ngOnInit(): void {
-  }
-
 }
